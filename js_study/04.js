4124343afe4ba6782343aac85920a09c65dc2e8d
jiny-01 <allie7019@naver.com>
2025-07-16 21:46:25 +0900

    feat: js_01~04

--- js_study/04.js
+++ js_study/04.js
@@ -34,6 +34,7 @@ console.log(obj1 === obj2); //메모리 주소(참조) 비교- 따로 만들었�
 //Json.parse() => JSON 문자열을 자바스크립트 객체 또는 배열을 변환
 // stringify <-> parse
 //
+console.log("--배열과 JSON--")
 const json1 = JSON.stringify(arr1);
 const json2 = JSON.stringify(arr2);
 console.log(json1);
@@ -47,4 +48,28 @@ const json4 = JSON.stringify(obj2);
 console.log(json3);
 console.log(json4);
 console.log(json3 === json4);  // -> true
+
+//배열의 다양한 기본 내장함수
+console.log("--배열의 기본 함수--")
+const names = ["이동윤", "삼동윤", "사동윤"];
+names.push("오동윤")
+console.log(names);
+// 상수인데 추가?
+// const 가 재할당을 금지하는 것일 뿐, 참조하는 객체(즉 배열)의 내용 변경까지 막는 것은 아님
+// names = ["육동윤"] 이건 재할당이므로 금지됨
+
+
+//요소 제거 : pop() => 배열의 마지막 요소를 제거하고 제거된 요소를 반환
+console.log(names.pop());
+
+//요소 수정/삽입/제거 : splice(삽입될 인덱스, 제거할 개수, 추가할 요소...)
+names.splice(1, 0, "육동윤");
+console.log(names);
+
+//요소 찾기 : find()
+const findFx = (n) => n === "육동윤";
+const foundName = names.find(findFx);
+//이걸 만족하는 첫번째 요소 반환
+//자바에서의 stream.filter -> 조건을 만족하는 것 반환
+console.log(foundName);
 
